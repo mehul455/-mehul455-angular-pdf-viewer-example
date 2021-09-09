11b856ae1f0d5657c34a5ac07bf4c6cb3cd86978
mehul455 <gmehul127@gmail.com>
2021-09-09 10:39:16 +0530

    pdf-viewer

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
+}
